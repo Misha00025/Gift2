05eeb4b8f5efeb3b2c7bcd2abad011df8f835485
Misha00025 <102412201+misha00025@users.noreply.github.com>
2026-01-27 14:38:26 +0300

    feat: health bar UI

--- Assets/Gift2/Characters/Character.cs
+++ Assets/Gift2/Characters/Character.cs
@@ -1,7 +1,21 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 
 public class Character : MonoBehaviour
 {
-    public int Health = 10;
+    [SerializeField] private int _health;
+
+    public int Health { 
+        get => _health;
+        set
+        {
+            _health = value;
+            HealthChanged.Invoke();
+        } 
+    }
+    
+    [field: SerializeField] public UnityEvent HealthChanged { get; private set; } = new UnityEvent();
+    
 }
