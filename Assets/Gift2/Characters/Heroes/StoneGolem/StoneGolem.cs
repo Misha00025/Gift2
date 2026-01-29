2335cf0cbb305c8cacb038ef7f05fd81bd828612
Misha00025 <102412201+misha00025@users.noreply.github.com>
2026-01-29 17:56:12 +0300

    feat: Stone golem base attack

--- Assets/Gift2/Characters/Heroes/StoneGolem/StoneGolem.cs
+++ Assets/Gift2/Characters/Heroes/StoneGolem/StoneGolem.cs
@@ -1,8 +1,20 @@
 public class StoneGolem : Character
 {
+    private Character _target;
+
     public override void Attack(Character target)
     {
-        throw new System.NotImplementedException();
+        _target = target;
+        Animator?.Play("BaseAttack");
+    }
+    
+    public void CompleteAttack()
+    {
+        if (_target == null) return;
+        
+        var damage = new Damage(){ Value = 5, Element = Element.Stone};
+        damage = _target.CalculateDamage(damage);
+        _target.ApplyDamage(damage);
     }
 
     public override Damage CalculateDamage(Damage damage)
