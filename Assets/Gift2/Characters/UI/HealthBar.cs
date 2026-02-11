ade90fd531a1b20b48db949542516e8945acac1b
Misha00025 <102412201+misha00025@users.noreply.github.com>
2026-02-11 15:50:17 +0300

    feat: property view

--- Assets/Gift2/Characters/UI/HealthBar.cs
+++ Assets/Gift2/Characters/UI/HealthBar.cs
@@ -1,27 +1,20 @@
 using UnityEngine;
 
-public class HealthBar : MonoBehaviour
+public class HealthBar : PropertyView
 {
     [SerializeField] private Character _character;
-    public SlicedFilledImage healthFiller;
     
     private int _maxValue;
     
     public void SetCharacter(Character character)
     {
         _character = character;
-        _maxValue = character.Health.MaxValue;
-        character.Health.Changed.AddListener((e) => ChangeFiller(e.Value));
+        SetProperty(_character.Health);        
     }
     
     public void Start()
     {
-        if (_character == null || healthFiller == null) return;
+        if (_character == null || Filler == null) return;
         SetCharacter(_character);
-    }
-    
-    private void ChangeFiller(int value)
-    {
-        healthFiller.fillAmount = ((float)value)/_maxValue;
     }
 }
---
+++ Assets/MyComponents/UI/PropertyView.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PropertyView : MonoBehaviour
+{
+    public SlicedFilledImage Filler;
+    
+    private Property _property;
+    
+    public void SetProperty(Property property)
+    {
+        if (_property != null)
+            _property.Changed.RemoveListener(OnChange);
+            
+        _property = property;
+        _property.Changed.AddListener(OnChange);
+    }
+    
+    protected virtual void OnChange(Property property)
+    {
+        Filler.fillAmount = ((float)property.Value)/property.MaxValue;
+    }
+}
