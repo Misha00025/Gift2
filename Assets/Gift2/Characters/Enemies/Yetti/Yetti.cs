ee797a5727c1d5ccd0dd282d3aca493b73c2789b
Misha00025 <102412201+misha00025@users.noreply.github.com>
2026-02-18 11:32:46 +0300

    feat: add Yetti

---
+++ Assets/Gift2/Characters/Enemies/Yetti/Yetti.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Yetti : Character
+{
+
+
+    public override void Attack()
+    {
+        var hit = PrepareHit(new Damage(){Value = Stats.damage, Element = Elements.Count > 0 ? Elements[0] : Element.Physical});
+        hit.Apply();
+        CompleteAttack();
+    }
+}
