3895bcd00fd75adb917e92a7537ac9738a168a42
Mikhail Nikolaev <102412201+Misha00025@users.noreply.github.com>
2026-03-07 15:24:40 +0300

    feat: shop completed

---
+++ Assets/Gift2/Scripts/UI/Shop/CostView.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gift2
+{
+    public class CostView : MonoBehaviour
+    {
+        public Image Icon;
+        public TextMeshProUGUI Amount;
+        
+        
+    }
+}
--- Assets/Gift2/Scripts/UI/Shop/ShopSlotView.cs
+++ Assets/Gift2/Scripts/UI/Shop/ShopSlotView.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,9 +12,11 @@ namespace Gift2
         public TextMeshProUGUI DescriptionField;
         public Transform CostsContainer;
         public Button BuyButton;
+        public CostView CostViewPrefab;
         
         private ShopController _controller;
         private int _index;
+        private List<CostView> _costViews = new();
         
         
         public void Initialize(ShopController controller, int slotIndex)
@@ -23,12 +26,15 @@ namespace Gift2
             
             BuyButton.onClick.AddListener(Buy);
             ReloadView();
+            ReloadCosts();
         }
         
         private void Buy()
         {
+            if (_controller.CanBuy(_index) == false) return;
+              
             _controller.Buy(_index);
-            ReloadView();
+            ReloadCosts();
         }
         
         private void ReloadView()
@@ -38,6 +44,22 @@ namespace Gift2
             NameField.SetText(slot.Name);
             DescriptionField.SetText(slot.Description);
             
+        }
+        
+        private void ReloadCosts()
+        {
+            var slot = _controller.Slots[_index];
+            
+            for (int i = 0; i < slot.Costs.Count; i++)
+            {
+                if (_costViews.Count <= i)
+                    _costViews.Add(Instantiate(CostViewPrefab, CostsContainer));
+                
+                var cost = slot.Costs[i];
+                var view = _costViews[i];
+                view.Amount.SetText(cost.Amount.ToString());
+                view.Icon.sprite = cost.Item.Icon;
+            }
         }
     }
 }
