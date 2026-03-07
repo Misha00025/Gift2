ac78ee92d6c5704838a9c1416987de614a49026f
Mikhail Nikolaev <102412201+Misha00025@users.noreply.github.com>
2026-03-07 18:42:38 +0300

    feat: add dialogue system plugin

---
+++ Assets/Plugins/HeneGames/Dialogue System/Scripts/DialogueManager.cs
@@ -0,0 +1,182 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace HeneGames.DialogueSystem
+{
+    public class DialogueManager : MonoBehaviour
+    {
+        private int currentSentence;
+        private float coolDownTimer;
+        private bool dialogueIsOn;
+
+        public enum TriggerState
+        {
+            Collision,
+            Input
+        }
+
+        [Header("References")]
+        [SerializeField] private AudioSource audioSource;
+
+        [Header("Events")]
+        public UnityEvent startDialogueEvent;
+        public UnityEvent nextSentenceDialogueEvent;
+        public UnityEvent endDialogueEvent;
+
+        [Header("Dialogue")]
+        [SerializeField] private TriggerState triggerState;
+        [SerializeField] private List<NPC_Centence> sentences = new List<NPC_Centence>();
+
+        private void Update()
+        {
+            //Timer
+            if(coolDownTimer > 0f)
+            {
+                coolDownTimer -= Time.deltaTime;
+            }
+        }
+
+        public void InitDialogue()
+        {
+            if (dialogueIsOn)
+                return;
+            startDialogueEvent.Invoke();
+
+            DialogueUI.instance.StartDialogue(this);
+
+            dialogueIsOn = true;
+        }
+
+        public void StartDialogue()
+        {
+            //Reset sentence index
+            currentSentence = 0;
+
+            //Show first sentence in dialogue UI
+            ShowCurrentSentence();
+
+            //Play dialogue sound
+            PlaySound(sentences[currentSentence].sentenceSound);
+
+            //Cooldown timer
+            coolDownTimer = sentences[currentSentence].skipDelayTime;
+        }
+
+        public void NextSentence(out bool lastSentence)
+        {
+            //The next sentence cannot be changed immediately after starting
+            if (coolDownTimer > 0f)
+            {
+                lastSentence = false;
+                return;
+            }
+
+            //Add one to sentence index
+            currentSentence++;
+
+            nextSentenceDialogueEvent.Invoke();
+
+            //If last sentence stop dialogue and return
+            if (currentSentence > sentences.Count - 1)
+            {
+                StopDialogue();
+
+                lastSentence = true;
+
+                endDialogueEvent.Invoke();
+
+                return;
+            }
+
+            //If not last sentence continue...
+            lastSentence = false;
+
+            //Play dialogue sound
+            PlaySound(sentences[currentSentence].sentenceSound);
+
+            //Show next sentence in dialogue UI
+            ShowCurrentSentence();
+
+            //Cooldown timer
+            coolDownTimer = sentences[currentSentence].skipDelayTime;
+        }
+
+        public void StopDialogue()
+        {
+            //Hide dialogue UI
+            DialogueUI.instance.ClearText();
+
+            //Stop audiosource so that the speaker's voice does not play in the background
+            if(audioSource != null)
+            {
+                audioSource.Stop();
+            }
+
+            //Remove trigger refence
+            dialogueIsOn = false;
+        }
+
+        private void PlaySound(AudioClip _audioClip)
+        {
+            //Play the sound only if it exists
+            if (_audioClip == null || audioSource == null)
+                return;
+
+            //Stop the audioSource so that the new sentence does not overlap with the old one
+            audioSource.Stop();
+
+            //Play sentence sound
+            audioSource.PlayOneShot(_audioClip);
+        }
+
+        private void ShowCurrentSentence()
+        {
+            if (sentences[currentSentence].dialogueCharacter != null)
+            {
+                //Show sentence on the screen
+                DialogueUI.instance.ShowSentence(sentences[currentSentence].dialogueCharacter, sentences[currentSentence].sentence);
+
+                //Invoke sentence event
+                sentences[currentSentence].sentenceEvent.Invoke();
+            }
+            else
+            {
+                DialogueCharacter _dialogueCharacter = new DialogueCharacter();
+                _dialogueCharacter.characterName = "";
+                _dialogueCharacter.characterPhoto = null;
+
+                DialogueUI.instance.ShowSentence(_dialogueCharacter, sentences[currentSentence].sentence);
+
+                //Invoke sentence event
+                sentences[currentSentence].sentenceEvent.Invoke();
+            }
+        }
+
+        public int CurrentSentenceLenght()
+        {
+            if(sentences.Count <= 0)
+                return 0;
+
+            return sentences[currentSentence].sentence.Length;
+        }
+    }
+
+    [System.Serializable]
+    public class NPC_Centence
+    {
+        [Header("------------------------------------------------------------")]
+
+        public DialogueCharacter dialogueCharacter;
+
+        [TextArea(3, 10)]
+        public string sentence;
+
+        public float skipDelayTime = 0.5f;
+
+        public AudioClip sentenceSound;
+
+        public UnityEvent sentenceEvent;
+    }
+}
